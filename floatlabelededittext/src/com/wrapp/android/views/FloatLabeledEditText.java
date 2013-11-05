164d32819e7303b617dd05eaca0a71b3ccff24f4
Henrik <sandstrom.henrik@gmail.com>
2013-11-05 14:55:19 -0800

    Initial Commit

---
+++ floatlabelededittext/src/com/wrapp/android/views/FloatLabeledEditText.java
@@ -0,0 +1,125 @@
+package com.wrapp.android.views;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.nineoldandroids.animation.Animator;
+import com.nineoldandroids.animation.AnimatorListenerAdapter;
+import com.nineoldandroids.animation.AnimatorSet;
+import com.nineoldandroids.animation.ObjectAnimator;
+import com.wrapp.android.R;
+
+public class FloatLabeledEditText extends LinearLayout {
+
+  private String hint;
+
+  private TextView hintTextView;
+  private EditText editText;
+
+  public FloatLabeledEditText(Context context) {
+    this(context, null);
+    initialize();
+  }
+
+  public FloatLabeledEditText(Context context, AttributeSet attrs) {
+    this(context, attrs, 0);
+  }
+
+  public FloatLabeledEditText(Context context, AttributeSet attrs, int defStyle) {
+    super(context, attrs, defStyle);
+
+    TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.FloatLabeledEditText);
+    try {
+      hint = a.getString(R.styleable.FloatLabeledEditText_floatingHint);
+    } finally {
+      a.recycle();
+    }
+
+    initialize();
+  }
+
+  private void initialize() {
+    setOrientation(VERTICAL);
+
+    View view = LayoutInflater.from(getContext()).inflate(R.layout.float_labeled_edit_text, this);
+
+    hintTextView = (TextView) view.findViewById(R.id.FloatLabeledEditTextHint);
+    editText = (EditText) view.findViewById(R.id.FloatLabeledEditTextEditText);
+
+    if (hint != null) {
+      setHint(hint);
+    }
+
+    hintTextView.setVisibility(View.INVISIBLE);
+    editText.addTextChangedListener(onTextChanged);
+  }
+
+  private TextWatcher onTextChanged = new TextWatcher() {
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+      setShowHint(editable.length() != 0);
+    }
+  };
+
+  private void setShowHint(final boolean show) {
+    AnimatorSet animation = null;
+    if ((hintTextView.getVisibility() == VISIBLE) && !show) {
+      animation = new AnimatorSet();
+      ObjectAnimator move = ObjectAnimator.ofFloat(hintTextView, "translationY", 0, hintTextView.getHeight() / 8);
+      ObjectAnimator fade = ObjectAnimator.ofFloat(hintTextView, "alpha", 1, 0);
+      animation.playTogether(move, fade);
+    } else if ((hintTextView.getVisibility() != VISIBLE) && show) {
+      animation = new AnimatorSet();
+      ObjectAnimator move = ObjectAnimator.ofFloat(hintTextView, "translationY", hintTextView.getHeight() / 8, 0);
+      ObjectAnimator fade = ObjectAnimator.ofFloat(hintTextView, "alpha", 0, 1);
+      animation.playTogether(move, fade);
+    }
+
+    if (animation != null) {
+      animation.addListener(new AnimatorListenerAdapter() {
+        @Override
+        public void onAnimationStart(Animator animation) {
+          super.onAnimationStart(animation);
+          hintTextView.setVisibility(VISIBLE);
+        }
+
+        @Override
+        public void onAnimationEnd(Animator animation) {
+          super.onAnimationEnd(animation);
+          hintTextView.setVisibility(show ? VISIBLE : INVISIBLE);
+        }
+      });
+      animation.start();
+    }
+  }
+
+  private void setHint(String hint) {
+    this.hint = hint;
+    editText.setHint(hint);
+    hintTextView.setText(hint);
+  }
+
+  public Editable getText() {
+    return editText.getText();
+  }
+
+  public void setText(CharSequence text) {
+    editText.setText(text);
+  }
+}
